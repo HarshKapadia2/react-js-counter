9ec987de18af97ecdd6c6780307a60c11404f98f
Harsh Kapadia <harshgkapadia@gmail.com>
2021-09-10 12:54:32 +0530

    :bug: bug: Remove persistent 0 while editing `Initializer.js` inputs directly

--- src/components/Initializer.js
+++ src/components/Initializer.js
@@ -2,22 +2,22 @@ import { useState } from "react";
 import "../css/initializer.css";
 
 const Initializer = ({ receiveMaxVal, receiveInitialVal }) => {
-	const [maxVal, setMaxVal] = useState(1000);
-	const [initialVal, setInitialVal] = useState(1);
+	const [maxVal, setMaxVal] = useState("1000");
+	const [initialVal, setInitialVal] = useState("1");
 
 	return (
 		<div id="initializer">
 			<div className="initializer-input-wrapper">
 				<input
 					id="initial-input"
-					onChange={(e) => setInitialVal(Number(e.target.value))}
+					onChange={(e) => setInitialVal(e.target.value)}
 					value={initialVal}
 					type="number"
 					placeholder="Enter initial value"
 				></input>
 				<button
 					className="btn"
-					onClick={() => receiveInitialVal(initialVal)}
+					onClick={() => receiveInitialVal(parseInt(initialVal))}
 				>
 					Set Initial Value
 				</button>
@@ -26,12 +26,15 @@ const Initializer = ({ receiveMaxVal, receiveInitialVal }) => {
 			<div className="initializer-input-wrapper">
 				<input
 					id="max-val-input"
-					onChange={(e) => setMaxVal(Number(e.target.value))}
+					onChange={(e) => setMaxVal(e.target.value)}
 					value={maxVal}
 					type="number"
 					placeholder="Enter maximum value"
 				></input>
-				<button className="btn" onClick={() => receiveMaxVal(maxVal)}>
+				<button
+					className="btn"
+					onClick={() => receiveMaxVal(parseInt(maxVal))}
+				>
 					Set Maximum Value
 				</button>
 			</div>
